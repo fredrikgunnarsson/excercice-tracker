b3e1d25516542262e7198eed0a1b82567b580385
Fredrik Gunnarsson <fredrik.s.gunnarsson@gmail.com>
2021-07-26 15:04:20 +0200

    lagt till några fler API-endpoints (exercices, logs)

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/api/users/:id", (req, res) => {
-  res.send("hello");
-  console.log("hello api");
-  console.log(req.params);
+  res.send(req.params);
 });
-app.get("/users/", (req, res) => {
+app.get("/api/users/", (req, res) => {
   res.send(Users.users);
 });
 app.post("/api/users/", (req, res) => {
@@ -26,6 +24,24 @@ app.post("/api/users/", (req, res) => {
   let newUser = new User(req.body.username);
   Users.users.push(newUser);
   res.send(newUser);
+});
+app.post("/api/users/:_id/exercises", (req, res) => {
+  if (!isUserId(req.params._id)) return res.send("no such user ID");
+  let user = Users.users.find((el) => el._id);
+  delete req.body[":_id"];
+  user.log.push(req.body);
+  console.log(req.params);
+  console.log(req.body);
+  res.send(user);
+});
+
+app.get("/api/users/:_id/logs", (req, res) => {
+  if (!isUserId(req.params._id)) return res.send("no such user ID");
+  let user = Users.users.find((el) => el._id === req.params._id);
+  user.count = user.log.length;
+  // console.log(user);
+  // console.log(Users);
+  res.send(user);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
@@ -39,11 +55,15 @@ function generateId() {
 function isUser(user) {
   return Users.users.find((el) => el.username === user);
 }
+function isUserId(userId) {
+  console.log(Users.users.find((el) => el._id === userId));
+  return Users.users.find((el) => el._id === userId);
+}
 
 const Users = { users: [] };
 class User {
-  constructor(username, _id) {
-    (this.username = username), (this._id = generateId());
+  constructor(username, _id, log) {
+    (this.username = username), (this._id = generateId()), (this.log = []);
   }
 }
 Users.users.push(new User("stefan"));
